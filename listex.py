755be864a41c0c3fdcfbdd91e0301b2b303eb131
Ganeshmahalkar <navnathm2104@gmail.com>
2025-05-06 13:34:36 +0530

    new listex file add

--- listex.py
+++ listex.py
@@ -1,6 +1,10 @@
-list1 = [1,6,3,2,1,4,5,3]
-list2 = []
-for i in list1:
-    if i not in list2:
-        list2.append(i)
-print(list2)
+# list1 = [1,6,3,2,1,4,5,3]
+# list2 = []
+# for i in list1:
+#     if i not in list2:
+#         list2.append(i)
+# print(list2)
+
+l1 = [1,2,3,4,5]
+sqr_list = list(map(lambda x : x**2, l1))
+print(sqr_list)
